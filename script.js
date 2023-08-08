36d7a3d2847b48ed3cd34197528039be5ededf9e
Phan Ngọc Lunah Hà <ngochaphan.work@gmail.com>
2023-08-08 09:19:05 +0700

    Added my project

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSideBar() {
+    document.getElementById('sidebar').style.display = "block";
+    document.getElementById('bx-menu').style.display = "none";
+    document.getElementById('bx-x').style.display = "block";
+}
+
+function hideSideBar() {
+    document.getElementById('sidebar').style.display = "none";
+    document.getElementById('bx-menu').style.display = "block";
+    document.getElementById('bx-x').style.display = "none";
+}
